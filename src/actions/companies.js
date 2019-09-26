d4b06ee547222ef5e09099c442dde260a1bd032e
Erin Stalzer <erinstalzer@gmail.com>
2019-09-26 13:23:48 -0400

    setup addCompany

--- src/actions/companies.js
+++ src/actions/companies.js
@@ -1,12 +1,23 @@
 export const getCompanies = () => {
     return (dispatch) => {
-        dispatch({type: 'LOADING_COMAPNIES'})
+        dispatch({type: 'LOADING_COMPANIES'})
         return fetch('./companies')
         .then(resp => resp.json())
         .then(companies => dispatch({type: 'FETCH_COMPANIES', payload: companies}))
     }
 }
 
-export const addCompany = () => {
-    
+export const addCompany = (company) => {
+    return (diapatch) => {
+        dispatch({type: 'ADD_COMPANY', company})
+        return fetch('/companies', {
+            method: 'POST',
+            body: JSON.stringify(company),
+            headers: {
+                "Content-Type": 'application/json'
+            }
+        })
+        .then(resp => resp.json())
+        .then(company => dispatch({type: 'COMPANY_ADDED', payload: company}))
+    }
 }
