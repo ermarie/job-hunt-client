9a5e3f0e2a2192e795352d1c37789e663b3a6a84
Erin Stalzer <erinstalzer@gmail.com>
2019-09-26 11:50:36 -0400

    set up loading companies and fetch companies

---
+++ src/actions/companies.js
@@ -0,0 +1,8 @@
+export const getCompanies = () => {
+    return (dispatch) => {
+        dispatch({type: 'LOADING_COMAPNIES'})
+        return fetch('./companies')
+        .then(resp => resp.json())
+        .then(companies => dispatch({type: 'FETCH_COMPANIES', payload: companies}))
+    }
+}
--- src/reducers/companyReducer.js
+++ src/reducers/companyReducer.js
@@ -1,6 +1,16 @@
 export default (state = {companies: [], loading: false}, action) => {
     switch(action.type) {
+        case 'LOADING_COMPANIES':
+            return {
+                ...state, 
+                loadingCompanies:true
+            }
+        case 'FETCH_COMPANIES':
+            return {
+                companies: action.payload,
+                loadingCompanies: false
+            }
         default: 
-            return state;
+            return state
     }
 }
