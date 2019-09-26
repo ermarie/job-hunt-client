2d9637bd36c4f9135f9bbe982104a783e78d3fa1
Erin Stalzer <erinstalzer@gmail.com>
2019-09-26 11:04:21 -0400

    First commit

---
+++ src/reducers/companyReducer.js
@@ -0,0 +1,6 @@
+export default (state = {companies: [], loading: false}, action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import companyReducer from './companyReducer'
+
+const rootReducer = combineReducers({
+    companyReducer
+})
+
+export default rootReducer
